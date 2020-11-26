c56a32a825b18af3fbb0934c3ea255436969f4af
Patryk Wojcieszak <patryk.p.wojcieszak@gmail.com>
2020-11-26 22:29:39 +0100

    feat: added launch details component

--- src/modules/home/home.tsx
+++ src/modules/home/home.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import Nav from "../shared/nav/nav";
@@ -6,10 +6,13 @@ import Countdown from "./countdown/countdown";
 import RecentLaunches from "./recentLaunches/recentLaunches";
 import UpcomingLaunches from "../shared/upcomingLaunches/upcomingLaunches";
 import Footer from "../shared/footer/footer";
+import LaunchDetails from "./launchDetails/launchDetails";
 
 import styles from "./home.module.scss";
 
-const home = () => {
+const Home = () => {
+  const [showLaunchDetails, setShowLaunchDetails] = useState(false);
+
   return (
     <div className={styles.Home}>
       <div className={styles.Top}>
@@ -20,10 +23,16 @@ const home = () => {
             <h2 className={styles.LaunchName}>SENTINEL-6 Michael Freilich</h2>
           </div>
           <Countdown />
-          <div className={styles.ShowMore}>
-            <FontAwesomeIcon icon="arrow-down" />
-            <h4>SHOW DETAILS</h4>
-          </div>
+          {showLaunchDetails ? null : (
+            <div className={styles.ShowMore}>
+              <FontAwesomeIcon
+                icon="arrow-down"
+                onClick={() => setShowLaunchDetails(!showLaunchDetails)}
+              />
+              <h4>SHOW DETAILS</h4>
+            </div>
+          )}
+          {showLaunchDetails ? <LaunchDetails /> : null}
         </div>
       </div>
       <div className={styles.Home__Content}>
@@ -35,4 +44,4 @@ const home = () => {
   );
 };
 
-export default home;
+export default Home;
---
+++ src/modules/home/launchDetails/launchDetails.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+import styles from "./launchDetails.module.scss";
+
+const launchDetails = () => {
+  return (
+    <div className={styles.LaunchDetails}>
+      <div className={styles.TitleContainer}>
+        <h3>FLIGHT:</h3>
+        <h3>LAUNCH DATE:</h3>
+        <h3>ROCKET:</h3>
+        <h3>LAUNCH SITE:</h3>
+        <h3>DETAILS:</h3>
+      </div>
+      <div className={styles.ValuesContainer}>
+        <h3>#108</h3>
+        <h3>Sat Nov 21 2020 18:17:00 GMT+0100</h3>
+        <h3>Falcon 9</h3>
+        <h3>Vandenberg Air Force Base Space Launch Complex 4E</h3>
+        <h3>
+          SpaceX's 20th and final Crew Resupply Mission under the original NASA
+          CRS contract, this mission brings essential supplies to the
+          International Space Station using SpaceX's reusable Dragon spacecraft.
+          It is the last scheduled flight of a Dragon 1 capsule. (CRS-21 and up
+          under the new Commercial Resupply Services 2 contract will use Dragon
+          2.) The external payload for this mission is the Bartolomeo ISS
+          external payload hosting platform. Falcon 9 and Dragon will launch
+          from SLC-40, Cape Canaveral Air Force Station and the booster will
+          land at LZ-1. The mission will be complete with return and recovery of
+          the Dragon capsule and down cargo.
+        </h3>
+      </div>
+    </div>
+  );
+};
+
+export default launchDetails;
