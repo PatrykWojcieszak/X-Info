a79504106e3efa6dfee7eb3f222ec526003df9aa
Patryk Wojcieszak <patryk.p.wojcieszak@gmail.com>
2020-12-03 15:11:27 +0100

    refactor: code cleanups

--- src/modules/Rocket/Rocket.tsx
+++ src/modules/Rocket/Rocket.tsx
@@ -10,32 +10,6 @@ import falconHeavy_bg from "../../resources/images/fheavy_bg2.jpg";
 import falconheavy from "../../resources/images/falconHeavy.png";
 import Gallery from "../shared/Gallery/Gallery";
 
-const variants = {
-  enter: (direction: number) => {
-    return {
-      x: direction > 0 ? 1000 : -1000,
-      opacity: 0,
-    };
-  },
-  center: {
-    zIndex: 1,
-    x: 0,
-    opacity: 1,
-  },
-  exit: (direction: number) => {
-    return {
-      zIndex: 0,
-      x: direction < 0 ? 1000 : -1000,
-      opacity: 0,
-    };
-  },
-};
-
-const swipeConfidenceThreshold = 10000;
-const swipePower = (offset: number, velocity: number) => {
-  return Math.abs(offset) * velocity;
-};
-
 const images = [
   "https://cdni0.trtworld.com/w960/h540/q75/76923_USASpaceX_1587156063102.jpeg",
   "https://mk0spaceflightnoa02a.kinstacdn.com/wp-content/uploads/2020/01/49399916862_cd676f67f6_o-copy.jpg",
