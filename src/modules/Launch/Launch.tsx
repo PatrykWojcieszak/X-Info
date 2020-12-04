962198e1498f2b63a0d49b5edcb6d8748e0ab127
Patryk Wojcieszak <patryk.p.wojcieszak@gmail.com>
2020-12-04 16:07:21 +0100

    feat: added socials icons

--- src/modules/Launch/Launch.tsx
+++ src/modules/Launch/Launch.tsx
@@ -1,10 +1,19 @@
 import React from "react";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import LaunchExtendedInfo from "../shared/LaunchExtendedInfo/LaunchExtendedInfo";
 
 import fhheavy from "../../resources/images/falconHeavy.png";
 import styles from "./Launch.module.scss";
 import InfoLine from "../shared/InfoLine/InfoLine";
+import Gallery from "../shared/Gallery/Gallery";
+
+const images = [
+  "https://cdni0.trtworld.com/w960/h540/q75/76923_USASpaceX_1587156063102.jpeg",
+  "https://mk0spaceflightnoa02a.kinstacdn.com/wp-content/uploads/2020/01/49399916862_cd676f67f6_o-copy.jpg",
+  "https://e3.365dm.com/20/11/768x432/skynews-spacex-falcon-9-rocket_5173738.jpg?20201116003945",
+  "https://highxtar.com/wp-content/uploads/2020/05/highxtar-spacex-elon-musk-occupy-mars2.jpg",
+  "https://www.nasa.gov/sites/default/files/thumbnails/image/crew-1_certification_feature_main.jpg",
+];
 
 const Launch = () => {
   return (
@@ -32,6 +41,33 @@ const Launch = () => {
             <InfoLine title="MASS" value="70 kg | 229.6 lb" />
             <InfoLine title="MASS" value="70 kg | 229.6 lb" />
           </div>
+        </div>
+      </div>
+      <Gallery images={images} />
+      <div className={styles.SocialsContainer}>
+        <div className={styles.Icon}>
+          <FontAwesomeIcon icon={["fab", "reddit-alien"]} />
+          <h4>CAMPAIGN</h4>
+        </div>
+        <div className={styles.Icon}>
+          <FontAwesomeIcon icon={["fab", "reddit-alien"]} />
+          <h4>LAUNCH</h4>
+        </div>
+        <div className={styles.Icon}>
+          <FontAwesomeIcon icon={["fab", "reddit-alien"]} />
+          <h4>MEDIA</h4>
+        </div>
+        <div className={styles.Icon}>
+          <FontAwesomeIcon icon={["fab", "wikipedia-w"]} />
+          <h4>WIKIPEDIA</h4>
+        </div>
+        <div className={styles.Icon}>
+          <FontAwesomeIcon icon="file-alt" />
+          <h4>ARTICLE</h4>
+        </div>
+        <div className={styles.Icon}>
+          <FontAwesomeIcon icon="newspaper" />
+          <h4>PRESS KIT</h4>
         </div>
       </div>
     </div>
