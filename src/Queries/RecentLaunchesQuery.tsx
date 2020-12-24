b67462ff9153276b6b328b4a9f4c383a3becd374
Patryk Wojcieszak <patryk.1996.19@o2.pl>
2020-12-24 11:59:48 +0100

    fix: recent launches date from

--- src/Queries/RecentLaunchesQuery.tsx
+++ src/Queries/RecentLaunchesQuery.tsx
@@ -2,7 +2,7 @@ const RecentLaunchesQuery = {
   query: {
     date_utc: {
       $gte: "2020-06-06T00:00:00.000Z",
-      $lte: "2020-12-06T00:00:00.000Z",
+      $lte: new Date(),
     },
   },
   options: {
