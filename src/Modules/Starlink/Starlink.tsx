3cd7d371476ad5dade8f4a518cad38a88207bc8b
Patryk Wojcieszak <patryk.1996.19@o2.pl>
2020-12-26 16:26:27 +0100

    feat: added starlink map

--- src/Modules/Starlink/Starlink.tsx
+++ src/Modules/Starlink/Starlink.tsx
@@ -3,10 +3,11 @@ import axios from "axios";
 import { getLatLngObj } from "tle.js";
 import IStarlink from "../../Models/IStarlink";
 import Globe from "react-globe.gl";
+import * as THREE from "three";
+import IQueryResult from "../../Models/IQueryResult";
+import StarlinkQuery from "../../Queries/StarlinkQuery";
 
-var ReactTHREE = require("react-three");
-var THREE = require("three");
-
+import styles from "./Starlink.module.scss";
 interface ITest {
   lat: number;
   lng: number;
@@ -16,74 +17,69 @@ interface ITest {
 }
 
 const Starlink = () => {
-  const [starlink, setStarlink] = useState<IStarlink | undefined>(undefined);
+  const [starlink, setStarlink] = useState<IQueryResult<IStarlink> | undefined>(
+    undefined
+  );
 
   useEffect(() => {
-    axios
-      .get<IStarlink>(
-        "https://api.spacexdata.com/v4/starlink/5eed770f096e59000698560d"
-      )
-      .then((res) => {
-        setStarlink(res.data);
-        console.log(res.data);
-      })
-      .catch((err) => {});
+    const interval = setInterval(() => {
+      axios
+        .post<IQueryResult<IStarlink>>(
+          "https://api.spacexdata.com/v4/starlink/query",
+          StarlinkQuery
+        )
+        .then((res) => {
+          setStarlink(res.data);
+        })
+        .catch((err) => {});
+    }, 3000);
+
+    return () => {
+      clearInterval(interval);
+    };
   }, []);
 
+  const gData: ITest[] = [];
   if (starlink) {
-    const tle = `STARLINK-76
-    1 44287U 19029BE  20359.39063386  .00003809  00000-0  86446-4 0  9995
-    2 44287  52.9981  21.7980 0002221  70.6436 289.4810 15.45669892 89555`;
-    const TLE0 = starlink?.spaceTrack.TLE_LINE0.substring(
-      2,
-      starlink?.spaceTrack.TLE_LINE0.length
-    );
-    console.log(TLE0);
-    // const tle =
-    //   TLE0 +
-    //   " \n" +
-    //   starlink?.spaceTrack.TLE_LINE1 +
-    //   "\n" +
-    //   starlink?.spaceTrack.TLE_LINE2;
+    starlink.docs.map((starlink) => {
+      const TLE0 = starlink?.spaceTrack.TLE_LINE0.substring(
+        2,
+        starlink?.spaceTrack.TLE_LINE0.length
+      );
 
-    console.log(tle);
-    const latLonObj = getLatLngObj(tle);
-    console.log(latLonObj);
+      const tle =
+        TLE0 +
+        " \n" +
+        starlink?.spaceTrack.TLE_LINE1 +
+        "\n" +
+        starlink?.spaceTrack.TLE_LINE2;
+
+      const latLonObj = getLatLngObj(tle);
+
+      gData.push({
+        lat: latLonObj.lat,
+        lng: latLonObj.lng,
+        alt: 0.9,
+        radius: 0.01,
+        color: "white",
+      });
+    });
   }
 
-  const globeEl = useRef();
-  const [data, setData] = useState<ITest[]>([
-    {
-      lat: 52.39078,
-      lng: 53.4922,
-      alt: 550,
-      radius: 10,
-      color: "red",
-    },
-  ]);
+  let globe = <></>;
 
-  useEffect(() => {
-    (function moveSpheres() {
-      data.forEach((d) => (d.lat += 0.2));
-      setData(data.slice());
-      requestAnimationFrame(moveSpheres);
-    })();
-  }, []);
+  if (gData.length > 0) {
+    globe = (
+      <Globe
+        globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
+        pointsData={gData}
+        pointAltitude={0.001}
+        pointColor="color"
+      />
+    );
+  }
 
-  return (
-    <Globe
-      ref={globeEl}
-      globeImageUrl="//unpkg.com/three-globe/example/img/earth-blue-marble.jpg"
-      bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
-      customLayerData={data}
-      customThreeObject={(d: any) =>
-        new THREE.Mesh(
-          new THREE.SphereBufferGeometry(d.radius),
-          new THREE.MeshLambertMaterial({ color: d.color })
-        )
-      }
-    />
-  );
+  return <div className={styles.Starlink}>{globe}</div>;
 };
 
 export default Starlink;
---
+++ src/Queries/StarlinkQuery.ts
@@ -0,0 +1,12 @@
+const StarlinkQuery = {
+  query: {
+    longitude: {
+      $ne: null,
+    },
+  },
+  options: {
+    limit: 1000,
+  },
+};
+
+export default StarlinkQuery;
