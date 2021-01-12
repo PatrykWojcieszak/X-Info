96f7c739d14e63c03b29213fec22959729ede62c
Patryk Wojcieszak <patryk.1996.19@o2.pl>
2021-01-12 13:51:39 +0100

    refactor: change file types

---
+++ src/Queries/BoostersQuery.ts
@@ -0,0 +1,26 @@
+const BoostersQuery = {
+  query: {},
+  options: {
+    limit: 10,
+    page: 1,
+    select: {
+      block: 1,
+      status: 1,
+      launches: 1,
+      serial: 1,
+      reuse_count: 1,
+    },
+    populate: [
+      {
+        path: "launches",
+        select: {
+          name: 1,
+          flight_number: 1,
+          date_utc: 1,
+        },
+      },
+    ],
+  },
+};
+
+export default BoostersQuery;
