53dfe5cd2320b2e6b2743ac946ba123df8596789
Patryk Wojcieszak <patryk.1996.19@o2.pl>
2021-01-16 21:35:02 +0100

    feat: added l18n package adn config file

---
+++ src/i18n.ts
@@ -0,0 +1,21 @@
+import i18n from "i18next";
+import Backend from "i18next-xhr-backend";
+import LanguageDetector from "i18next-browser-languagedetector";
+import { initReactI18next } from "react-i18next";
+
+i18n
+  .use(Backend)
+  // detect user language
+  // learn more: https://github.com/i18next/i18next-browser-languageDetector
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    fallbackLng: "de",
+    debug: true,
+
+    interpolation: {
+      escapeValue: false,
+    },
+  });
+
+export default i18n;
