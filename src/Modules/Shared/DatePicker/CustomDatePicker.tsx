ae96431f175038ae741fc65d47fac9013cc332e6
Patryk Wojcieszak <patryk.1996.19@o2.pl>
2021-01-22 15:28:31 +0100

    feat: addec possiblity to input date from keyboard

--- src/Modules/Shared/DatePicker/CustomDatePicker.tsx
+++ src/Modules/Shared/DatePicker/CustomDatePicker.tsx
@@ -6,17 +6,22 @@ import styles from "./CustomDatePicker.module.scss";
 import "react-datepicker/dist/react-datepicker.css";
 
 const CustomDatePicker = ({ date, dateChanged }: datePickerProps) => {
-  const DatePickerCustomInput = ({ value, onClick }) => (
-    <div className={styles.DatePicker} onClick={onClick}>
-      <h3>{value}</h3>
-    </div>
-  );
+  const CustomInput = (
+    props: React.HTMLProps<HTMLInputElement>,
+    ref: React.Ref<HTMLInputElement>
+  ) => {
+    return (
+      <div className={styles.DatePicker}>
+        <input {...props} ref={props.ref} />
+      </div>
+    );
+  };
 
   return (
     <DatePicker
       selected={date}
       onChange={(date) => dateChanged(date)}
-      customInput={<DatePickerCustomInput value={date} onClick={() => {}} />}
+      customInput={React.createElement(React.forwardRef(CustomInput))}
     />
   );
 };
