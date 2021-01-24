8dee45bc524b73b2d4ab68845aad576a8a64f134
Patryk Wojcieszak <patryk.1996.19@o2.pl>
2021-01-24 18:07:57 +0100

    feat: translated launch module

--- src/Modules/Launch/Ship/Ship.tsx
+++ src/Modules/Launch/Ship/Ship.tsx
@@ -1,14 +1,17 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
 
 //STYLES
 import styles from "./Ship.module.scss";
 
 const Ship = ({ name, img }: shipProps) => {
+  const { t } = useTranslation();
+
   return (
     <div className={styles.Ship}>
       <img src={img} alt="ship" loading="lazy" />
       <h3>
-        NAME: <span>{name}</span>
+        {t("name")}: <span>{name}</span>
       </h3>
     </div>
   );
