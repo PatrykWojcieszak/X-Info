d46f80332eda74a392e77d908f1dc74475a1d252
Patryk Wojcieszak <patryk.1996.19@o2.pl>
2021-01-25 11:45:19 +0100

    refactor: changed to hashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
 import configureStore from "./Store/configureStore";
 import "./fontawesome/fontawesome";
