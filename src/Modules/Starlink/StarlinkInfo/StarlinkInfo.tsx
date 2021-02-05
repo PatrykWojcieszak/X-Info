ce9c11e1f26a41d6c927effaa6f3529f7e2c79cd
Patryk Wojcieszak <patryk.1996.19@o2.pl>
2021-02-05 14:38:53 +0100

    refactor: refactored starlink

--- src/Modules/Starlink/StarlinkInfo/StarlinkInfo.tsx
+++ src/Modules/Starlink/StarlinkInfo/StarlinkInfo.tsx
@@ -1,8 +1,10 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { motion } from "framer-motion";
-import { useTranslation } from "react-i18next";
+
+//COMPONENTS
+import { StarlinkDetails } from "./StarlinkDetails/StarlinkDetails";
+import { MissionDetails } from "./MissionDetails/MissionDetails";
 
 //TYPES
 import { GlobePoint } from "../../../Types";
@@ -12,8 +14,6 @@ import styles from "./StarlinkInfo.module.scss";
 import { pageVariantsAnim } from "../../../Animations/Animations_motion";
 
 export const StarlinkInfo = React.memo(({ starlink, close }: starlinkProps) => {
-  const { t } = useTranslation();
-
   return (
     <motion.div
       initial="initial"
@@ -22,35 +22,17 @@ export const StarlinkInfo = React.memo(({ starlink, close }: starlinkProps) => {
       variants={pageVariantsAnim}
       className={styles.StarlinkInfo}>
       <FontAwesomeIcon onClick={close} icon="times" />
-      <div className={styles.Left}>
-        <h2>STARLINK</h2>
-        <div className={styles.InfoContainer}>
-          <div className={styles.NameWrapper}>
-            <h4>{t("name")}</h4>
-            <h4>{t("version")}</h4>
-            <h4>{t("height")}</h4>
-            <h4>{t("velocity")}</h4>
-          </div>
-          <div className={styles.ValuesWrapper}>
-            <h4>{starlink.label}</h4>
-            <h4>{starlink.version}</h4>
-            <h4>{Number(starlink.height_km).toFixed(2)} km</h4>
-            <h4>{Number(starlink.velocity_kms).toFixed(2)} kms</h4>
-          </div>
-        </div>
-      </div>
-      <div className={styles.Right}>
-        <h2>{t("launch")}</h2>
-        <Link to={`launch/${starlink.launch?.flight_number}`}>
-          <div className={styles.LaunchInfoWrapper}>
-            <h3>{starlink.launch?.name}</h3>
-            <h4>
-              {t("key", { date: new Date(starlink.launch?.date_utc) })} | #
-              {starlink.launch?.flight_number}
-            </h4>
-          </div>
-        </Link>
-      </div>
+      <StarlinkDetails
+        label={starlink.label}
+        version={starlink.version}
+        height={Number(starlink.height_km)}
+        velocity={Number(starlink.velocity_kms)}
+      />
+      <MissionDetails
+        name={starlink.launch.name}
+        dateUtc={starlink.launch.date_utc}
+        flightNumber={starlink.launch.flight_number}
+      />
     </motion.div>
   );
 });
