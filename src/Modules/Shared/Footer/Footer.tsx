fd374cbc9fa1d2f432dc405dec28d930230b0b0a
Patryk Wojcieszak <patryk.1996.19@o2.pl>
2021-02-05 14:03:58 +0100

    refactor: refactored footer

--- src/Modules/Shared/Footer/Footer.tsx
+++ src/Modules/Shared/Footer/Footer.tsx
@@ -1,74 +1,17 @@
-import React, { useRef, useState } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useTranslation } from "react-i18next";
+import React from "react";
+
+//COMPONENTS
+import { Author } from "./Author/Author";
+import { LngSelector } from "./LngSelector/LngSelector";
 
 //STYLES
 import styles from "./Footer.module.scss";
-import { useClickOutside } from "../../../Hooks/useClickOutside";
 
 export const Footer = () => {
-  const { i18n, t } = useTranslation();
-
-  const [selectedLang, setSelectedLang] = useState(i18n.language);
-  const [showLngSelector, setShowLngSelector] = useState(false);
-
-  const changeLanguageHandler = (lng) => {
-    setSelectedLang(lng);
-    i18n.changeLanguage(lng);
-    setShowLngSelector(false);
-  };
-
-  const closeShowLngSelectorHandler = () => {
-    setShowLngSelector(false);
-  };
-
-  const wrapperRef = useRef(null);
-  useClickOutside(wrapperRef, closeShowLngSelectorHandler);
-
   return (
     <div className={styles.Footer}>
-      <h3>
-        {t("createdBy")}:{" "}
-        <a
-          href="https://github.com/PatrykWojcieszak"
-          target="_blank"
-          rel="noopener noreferrer">
-          {" "}
-          Patryk Wojcieszak
-        </a>{" "}
-        {t("using")}{" "}
-        <a
-          href="https://github.com/r-spacex/SpaceX-API"
-          target="_blank"
-          rel="noopener noreferrer">
-          SpaceX-Api
-        </a>
-      </h3>
-      <div ref={wrapperRef} className={styles.LanguageSelector}>
-        <div
-          style={{ display: "flex", alignItems: "center" }}
-          onClick={() => setShowLngSelector(!showLngSelector)}>
-          <FontAwesomeIcon icon="globe-americas" />
-          {selectedLang}
-        </div>
-        <div
-          className={
-            showLngSelector
-              ? [styles.LanguagesWrapper, styles.Show].join(" ")
-              : styles.LanguagesWrapper
-          }>
-          <h4
-            className={selectedLang === "en" ? styles.Selected : ""}
-            onClick={() => changeLanguageHandler("en")}>
-            English - EN
-          </h4>
-          <h4
-            className={selectedLang === "pl" ? styles.Selected : ""}
-            onClick={() => changeLanguageHandler("pl")}>
-            Polish - PL
-          </h4>
-        </div>
-      </div>
+      <Author />
+      <LngSelector />
     </div>
   );
 };
