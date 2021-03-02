bfcf43c725c892845785413864bd27b350923b35
Patryk Wojcieszak <patryk.1996.19@o2.pl>
2021-03-02 14:24:44 +0100

    style: changed scss to styledComponets in button

--- src/Modules/Shared/Button/Button.tsx
+++ src/Modules/Shared/Button/Button.tsx
@@ -1,23 +1,17 @@
 import React from "react";
-
-//STYLES
-import styles from "./Button.module.scss";
+import styled from "styled-components/macro";
+import { device } from "../../../resources/styles/helpers/breakpoints";
 
 export const Button = React.memo(
   ({ name, selected, clicked, disabled, styleType }: buttonProps) => {
-    const btnStyle = [styles.Button];
-
-    if (styleType === "primary") btnStyle.push(styles.Primary);
-    if (styleType === "secondary") btnStyle.push(styles.Secondary);
-    if (selected) btnStyle.push(styles.Selected);
-
     return (
-      <button
+      <StyledButton
+        styleType={styleType}
+        selected={selected}
         disabled={disabled}
-        onClick={clicked}
-        className={btnStyle.join(" ")}>
+        onClick={clicked}>
         {name}
-      </button>
+      </StyledButton>
     );
   }
 );
@@ -29,3 +23,46 @@ type buttonProps = {
   disabled?: boolean;
   styleType: string;
 };
+
+const StyledButton = styled.button<{ styleType: string; selected?: boolean }>`
+  box-sizing: border-box;
+  outline: none;
+  border-radius: 0.4rem;
+  transition: all 0.3s ease-in-out;
+  cursor: pointer;
+  font-size: 0.8rem;
+  font-weight: 700;
+  padding: 0.6rem;
+  background: ${({ theme, selected }) =>
+    selected ? theme.colors?.blue : "transparent"};
+
+  border: 1px solid
+    ${({ theme, styleType }) =>
+      styleType === "primary" ? theme.colors?.blue : theme.colors?.background};
+
+  color: ${({ theme, styleType, selected }) =>
+    selected
+      ? theme.colors?.background
+      : styleType === "primary"
+      ? theme.colors?.blue
+      : theme.colors?.background};
+
+  &:hover {
+    background: ${({ theme, styleType }) =>
+      styleType === "primary" ? theme.colors?.blue : theme.colors?.background};
+
+    color: ${({ theme, styleType }) =>
+      styleType === "primary" ? theme.colors?.background : theme.colors?.blue};
+  }
+
+  @media ${device.tablet} {
+    font-size: 1rem;
+    font-weight: 300;
+  }
+
+  @media ${device.large} {
+    font-size: 1.3rem;
+    font-weight: 100;
+    padding: 1rem;
+  }
+`;
