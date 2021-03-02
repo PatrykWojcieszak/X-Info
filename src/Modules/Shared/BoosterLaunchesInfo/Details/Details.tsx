c6e2e4df188cf66f1e5a0194faf39ca70279f34e
Patryk Wojcieszak <patryk.1996.19@o2.pl>
2021-03-02 16:09:01 +0100

    style: changed scss to styledComponents in boosters

--- src/Modules/Shared/BoosterLaunchesInfo/Details/Details.tsx
+++ src/Modules/Shared/BoosterLaunchesInfo/Details/Details.tsx
@@ -7,6 +7,8 @@ import styles from "./Details.module.scss";
 //TYPES
 import { Launch } from "../../../../Types";
 import { InfoElement } from "./InfoElement/InfoElement";
+import styled from "styled-components/macro";
+import { device } from "../../../../resources/styles/helpers/breakpoints";
 
 export const Details = ({
   block,
@@ -18,25 +20,17 @@ export const Details = ({
   const { t } = useTranslation();
 
   return (
-    <div className={styles.Details}>
-      <div className={[styles.InfoElement, styles.One].join(" ")}>
-        <InfoElement title={t("block")} value={block.toString()} />
-      </div>
-      <div className={[styles.InfoElement, styles.Two].join(" ")}>
-        <InfoElement title={t("serialNumber")} value={serial} />
-      </div>
-      <div className={[styles.InfoElement, styles.Three].join(" ")}>
-        <InfoElement
-          title={t("launches")}
-          value={
-            reuse_count ? `${reuse_count + 1}` : launches.length > 0 ? "1" : "0"
-          }
-        />
-      </div>
-      <div className={[styles.InfoElement, styles.Four].join(" ")}>
-        <InfoElement title={t("status")} value={status} />
-      </div>
-    </div>
+    <StyledDetails>
+      <InfoElement title={t("block")} value={block.toString()} />
+      <InfoElement title={t("serialNumber")} value={serial} />
+      <InfoElement
+        title={t("launches")}
+        value={
+          reuse_count ? `${reuse_count + 1}` : launches.length > 0 ? "1" : "0"
+        }
+      />
+      <InfoElement title={t("status")} value={status} />
+    </StyledDetails>
   );
 };
 
@@ -47,3 +41,23 @@ type detailsProps = {
   reuse_count: number;
   launches: Launch[];
 };
+
+const StyledDetails = styled.div`
+  padding: 1rem;
+  border-radius: 1rem;
+  background-color: $foreground;
+  box-shadow: 0px 0px 16px 25px rgba(0, 0, 0, 0.55);
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-gap: 10px;
+  grid-auto-rows: minmax(80px, 20px);
+  font-size: 1rem;
+  font-weight: 300;
+  min-width: 100%;
+  max-width: 100%;
+
+  @media ${device.tablet} {
+    min-width: 320px;
+    max-width: 320px;
+  }
+`;
