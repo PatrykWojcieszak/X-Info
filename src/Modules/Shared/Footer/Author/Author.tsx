c85686b57bf73429d7c9131fba68de65816df024
Patryk Wojcieszak <patryk.1996.19@o2.pl>
2021-03-03 20:08:56 +0100

    style: changed scss to styledComponents in Footer

--- src/Modules/Shared/Footer/Author/Author.tsx
+++ src/Modules/Shared/Footer/Author/Author.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
+import styled from "styled-components/macro";
 
 //STYLES
 import styles from "./Author.module.scss";
@@ -8,7 +9,7 @@ export const Author = () => {
   const { t } = useTranslation();
 
   return (
-    <h3 className={styles.Author}>
+    <StyledAuthor>
       {t("createdBy")}:{" "}
       <a
         href="https://github.com/PatrykWojcieszak"
@@ -24,6 +25,19 @@ export const Author = () => {
         rel="noopener noreferrer">
         SpaceX-Api
       </a>
-    </h3>
+    </StyledAuthor>
   );
 };
+
+const StyledAuthor = styled.h3`
+  font-weight: 100;
+  z-index: 1000;
+  color: ${({ theme }) => theme.colors?.fontSecondary};
+
+  > a {
+    text-decoration: none;
+    color: ${({ theme }) => theme.colors?.blue};
+    cursor: pointer;
+    font-weight: 300;
+  }
+`;
