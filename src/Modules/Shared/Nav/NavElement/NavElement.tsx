94ec15803638cdaa38d9c32fab1561a5f2399e3a
Patryk Wojcieszak <patryk.1996.19@o2.pl>
2021-03-04 17:50:32 +0100

    style: changed scss to styledComponents in Nav

--- src/Modules/Shared/Nav/NavElement/NavElement.tsx
+++ src/Modules/Shared/Nav/NavElement/NavElement.tsx
@@ -1,19 +1,14 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-
-//STYLES
-import styles from "./NavElement.module.scss";
+import styled from "styled-components/macro";
+import { device } from "../../../../resources/styles/helpers/breakpoints";
 
 export const NavElement = React.memo(
   ({ name, link, exact }: navElementProps) => {
     return (
-      <NavLink
-        to={link}
-        exact={exact}
-        className={styles.NavElement}
-        activeClassName={styles.NavElement__Active}>
+      <StyledNavElement as={NavLink} to={link} exact={exact}>
         {name}
-      </NavLink>
+      </StyledNavElement>
     );
   }
 );
@@ -23,3 +18,62 @@ type navElementProps = {
   link: string;
   exact: boolean;
 };
+
+const StyledNavElement = styled(NavLink)`
+  position: relative;
+  overflow: hidden;
+  will-change: color;
+  transition: color 0.25s ease-out;
+  color: ${({ theme }) => theme.colors?.fontSecondary};
+  font-weight: 100;
+  border: none;
+  outline: none;
+  text-decoration: none;
+  font-size: 1.3rem;
+  margin: 0.4rem;
+  padding: 0.6rem 0.1rem;
+
+  &:hover {
+    cursor: pointer;
+  }
+
+  &.active {
+    border-bottom: 2px solid ${({ theme }) => theme.colors?.blue};
+  }
+
+  &::before {
+    left: 50%;
+    transform: translateX(-50%);
+  }
+
+  &::after {
+    right: 50%;
+    transform: translateX(50%);
+  }
+
+  &:hover::before,
+  &:hover::after {
+    width: 100%;
+    transition-duration: 0.2s;
+  }
+
+  &::before,
+  &::after {
+    content: "";
+    width: 0;
+    height: 2px;
+    background-color: ${({ theme }) => theme.colors?.blue};
+
+    will-change: width;
+    transition: width 0.15s ease-out;
+
+    position: absolute;
+    bottom: 0;
+  }
+
+  @media ${device.tablet} {
+    margin: 1.5rem;
+    padding: 0.6rem 0.2rem;
+    font-size: 1.6rem;
+  }
+`;
