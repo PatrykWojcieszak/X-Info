ee86a4231df0b21edda0c22aad713dcf0bf2344d
Patryk Wojcieszak <patryk.1996.19@o2.pl>
2021-03-05 13:08:12 +0100

    style: changed scss to styledComponents in Skeletons

--- src/Modules/Shared/Skeletons/LaunchSkeleton.tsx
+++ src/Modules/Shared/Skeletons/LaunchSkeleton.tsx
@@ -5,37 +5,67 @@ import { Shimmer } from "./Shimmer/Shimmer";
 import { SkeletonElement } from "./SkeletonElement";
 import { LaunchExtendedInfoSkeleton } from "./LaunchExtendedInfoSkeleton";
 
-//STYLES
-import styles from "./Skeletons.module.scss";
+import styled from "styled-components/macro";
+import { flexColumn } from "../../../resources/styles/helpers/mixins";
 
 export const LaunchSkeleton = () => {
   return (
-    <div className={styles.SkeletonWrapper}>
-      <div className={styles.LaunchSkeleton}>
+    <StyledSkeletonWrapper>
+      <StyledLaunchSkeleton>
         <LaunchExtendedInfoSkeleton />
-        <div className={styles.Row}>
-          <div className={styles.Left}>
+        <StyledRow>
+          <StyledLeft>
             <SkeletonElement type="Box" />
-          </div>
-          <div className={styles.Right}>
-            <div className={styles.InfoWrapper}>
+          </StyledLeft>
+          <StyledRight>
+            <StyledInfoWrapper>
               <SkeletonElement type="Title" />
               <SkeletonElement type="Text" />
               <SkeletonElement type="Text" />
               <SkeletonElement type="Text" />
-            </div>
-            <div className={styles.InfoWrapper}>
+            </StyledInfoWrapper>
+            <StyledInfoWrapper>
               <SkeletonElement type="Title" />
               <SkeletonElement type="Text" />
               <SkeletonElement type="Text" />
               <SkeletonElement type="Text" />
               <SkeletonElement type="Text" />
               <SkeletonElement type="Text" />
-            </div>
-          </div>
-        </div>
-      </div>
+            </StyledInfoWrapper>
+          </StyledRight>
+        </StyledRow>
+      </StyledLaunchSkeleton>
       <Shimmer />
-    </div>
+    </StyledSkeletonWrapper>
   );
 };
+
+const StyledSkeletonWrapper = styled.div`
+  position: relative;
+  overflow: hidden;
+  border-radius: 1rem;
+  background-color: rgba(9, 9, 9, 0.2);
+`;
+
+const StyledLaunchSkeleton = styled(flexColumn)`
+  margin-bottom: 3rem;
+`;
+
+const StyledRow = styled.div`
+  display: flex;
+  height: 700px;
+  margin-top: 3rem;
+`;
+
+const StyledLeft = styled.div`
+  width: 200px;
+`;
+
+const StyledRight = styled.div`
+  width: 100%;
+  margin-left: 4rem;
+`;
+
+const StyledInfoWrapper = styled(flexColumn)`
+  margin-top: 2rem;
+`;
--- src/Modules/Shared/Skeletons/RecentLaunchSkeleton.tsx
+++ src/Modules/Shared/Skeletons/RecentLaunchSkeleton.tsx
@@ -1,22 +1,34 @@
 import React from "react";
+import styled from "styled-components/macro";
+import { flexColumnCenter } from "../../../resources/styles/helpers/mixins";
 
 //COMPONENTS
 import { Shimmer } from "./Shimmer/Shimmer";
 import { SkeletonElement } from "./SkeletonElement";
 
-//STYLES
-import styles from "./Skeletons.module.scss";
-
 export const RecentLaunchSkeleton = () => {
   return (
-    <div className={styles.SkeletonWrapper}>
-      <div className={styles.RecentLaunchSkeleton}>
+    <StyledSkeletonWrapper>
+      <StyledRecentLaunchSkeleton>
         <SkeletonElement type="Avatar" />
         <SkeletonElement type="Title" />
         <SkeletonElement type="Text" />
         <SkeletonElement type="Title" />
-      </div>
+      </StyledRecentLaunchSkeleton>
       <Shimmer />
-    </div>
+    </StyledSkeletonWrapper>
   );
 };
+
+const StyledSkeletonWrapper = styled.div`
+  position: relative;
+  overflow: hidden;
+  border-radius: 1rem;
+  background-color: rgba(9, 9, 9, 0.2);
+`;
+
+const StyledRecentLaunchSkeleton = styled(flexColumnCenter)`
+  justify-content: space-evenly;
+  width: 240px;
+  height: 350px;
+`;
